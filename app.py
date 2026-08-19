f8978274c6bf182a07be270740bc2d111bb7233b
muhyi2828 <muhyi.ard@gmail.com>
2026-08-19 22:45:19 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Kasir RABAY", page_icon="🚀")
+
+st.title("🚀 Kasir Pintar RABAY CELL")
+st.write("Halo Bos! Ini adalah langkah awal aplikasi kasir kita.")
+st.success("Website sudah berhasil menyala dari HP!")
