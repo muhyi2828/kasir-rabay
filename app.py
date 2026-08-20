59cfad164f9798a84f9063dc3560b98811e04e93
muhyi2828 <muhyi.ard@gmail.com>
2026-08-20 07:26:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -376,7 +376,6 @@ with tab1:
 
         if sumber_gambar and st.button("🔍 AI SCAN OTOMATIS (+/-)", use_container_width=True, type="primary"):
             try:
-                # Placeholder Animasi Google Lens (Tanpa st.image tambahan)
                 lens_placeholder = st.empty()
                 img_temp = Image.open(sumber_gambar)
                 
@@ -393,7 +392,8 @@ with tab1:
                 """, unsafe_allow_html=True)
 
                 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
-                res = client.models.generate_content(model='gemini-3.6-flash', contents=[img_temp, "Tulis semua nominal transaksi beserta tandanya (+ atau -). Balas dengan format angka dipisah koma, contoh: +9067000,-75000,-5000000"])
+                # Menggunakan gemini-1.5-flash untuk kestabilan kuota
+                res = client.models.generate_content(model='gemini-1.5-flash', contents=[img_temp, "Tulis semua nominal transaksi beserta tandanya (+ atau -). Balas dengan format angka dipisah koma, contoh: +9067000,-75000,-5000000"])
                 
                 lens_placeholder.empty()
 
